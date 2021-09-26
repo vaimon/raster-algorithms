7981402745d6965e128389b6ac23d90d71a3ac70
Andrew Vaimon <niko142000@mail.ru>
2021-09-26 20:53:11 +0300

    more buttonssss

--- RasterAlgorithms/Form1.Designer.cs
+++ RasterAlgorithms/Form1.Designer.cs
@@ -35,69 +35,142 @@ namespace RasterAlgorithms
             this.buttonHighlight = new System.Windows.Forms.Button();
             this.buttonLine = new System.Windows.Forms.Button();
             this.buttonTriangle = new System.Windows.Forms.Button();
+            this.chooseFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.comboBoxLine = new System.Windows.Forms.ComboBox();
+            this.color1 = new System.Windows.Forms.PictureBox();
+            this.color2 = new System.Windows.Forms.PictureBox();
+            this.color3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color3)).BeginInit();
             this.SuspendLayout();
             // 
             // canvas
             // 
-            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.canvas.Location = new System.Drawing.Point(0, 60);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(1274, 740);
+            this.canvas.Size = new System.Drawing.Size(1300, 840);
             this.canvas.TabIndex = 0;
             this.canvas.TabStop = false;
             // 
             // buttonFillColor
             // 
-            this.buttonFillColor.Location = new System.Drawing.Point(128, 42);
+            this.buttonFillColor.Location = new System.Drawing.Point(144, 42);
             this.buttonFillColor.Name = "buttonFillColor";
             this.buttonFillColor.Size = new System.Drawing.Size(166, 44);
             this.buttonFillColor.TabIndex = 1;
             this.buttonFillColor.Text = "Залить цветом";
             this.buttonFillColor.UseVisualStyleBackColor = true;
+            this.buttonFillColor.Click += new System.EventHandler(this.buttonFillColor_Click);
             // 
             // buttonFillImage
             // 
-            this.buttonFillImage.Location = new System.Drawing.Point(300, 42);
+            this.buttonFillImage.Location = new System.Drawing.Point(316, 42);
             this.buttonFillImage.Name = "buttonFillImage";
             this.buttonFillImage.Size = new System.Drawing.Size(186, 44);
             this.buttonFillImage.TabIndex = 1;
             this.buttonFillImage.Text = "Залить картинкой";
             this.buttonFillImage.UseVisualStyleBackColor = true;
+            this.buttonFillImage.Click += new System.EventHandler(this.buttonFillImage_Click);
             // 
             // buttonHighlight
             // 
-            this.buttonHighlight.Location = new System.Drawing.Point(492, 42);
+            this.buttonHighlight.Location = new System.Drawing.Point(508, 42);
             this.buttonHighlight.Name = "buttonHighlight";
             this.buttonHighlight.Size = new System.Drawing.Size(195, 44);
             this.buttonHighlight.TabIndex = 1;
             this.buttonHighlight.Text = "Выделить границу";
             this.buttonHighlight.UseVisualStyleBackColor = true;
+            this.buttonHighlight.Click += new System.EventHandler(this.buttonHighlight_Click);
             // 
             // buttonLine
             // 
-            this.buttonLine.Location = new System.Drawing.Point(693, 42);
+            this.buttonLine.Location = new System.Drawing.Point(709, 42);
             this.buttonLine.Name = "buttonLine";
             this.buttonLine.Size = new System.Drawing.Size(201, 44);
             this.buttonLine.TabIndex = 1;
             this.buttonLine.Text = "Нарисовать отрезок";
             this.buttonLine.UseVisualStyleBackColor = true;
+            this.buttonLine.Click += new System.EventHandler(this.buttonLine_Click);
             // 
             // buttonTriangle
             // 
-            this.buttonTriangle.Location = new System.Drawing.Point(900, 42);
+            this.buttonTriangle.Location = new System.Drawing.Point(916, 42);
             this.buttonTriangle.Name = "buttonTriangle";
             this.buttonTriangle.Size = new System.Drawing.Size(242, 44);
             this.buttonTriangle.TabIndex = 1;
             this.buttonTriangle.Text = "Градиентный треугольник";
             this.buttonTriangle.UseVisualStyleBackColor = true;
+            this.buttonTriangle.Click += new System.EventHandler(this.buttonTriangle_Click);
+            // 
+            // chooseFileDialog
+            // 
+            this.chooseFileDialog.Filter = "Картинки|*.jpeg;*.jpg";
+            this.chooseFileDialog.InitialDirectory = "C:\\Users\\niko1\\Desktop\\images";
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.Color = System.Drawing.Color.DarkRed;
+            // 
+            // comboBoxLine
+            // 
+            this.comboBoxLine.Items.AddRange(new object[] {
+            "алгоритм Брезенхема",
+            "алгоритм Ву"});
+            this.comboBoxLine.Location = new System.Drawing.Point(709, 92);
+            this.comboBoxLine.Name = "comboBoxLine";
+            this.comboBoxLine.Size = new System.Drawing.Size(201, 33);
+            this.comboBoxLine.TabIndex = 2;
+            this.comboBoxLine.Text = "Алгоритм";
+            this.comboBoxLine.Visible = false;
+            this.comboBoxLine.SelectedIndexChanged += new System.EventHandler(this.comboBoxLine_SelectedIndexChanged);
+            // 
+            // color1
+            // 
+            this.color1.BackColor = System.Drawing.Color.Red;
+            this.color1.Location = new System.Drawing.Point(979, 92);
+            this.color1.Name = "color1";
+            this.color1.Size = new System.Drawing.Size(33, 33);
+            this.color1.TabIndex = 3;
+            this.color1.TabStop = false;
+            this.color1.Visible = false;
+            this.color1.Click += new System.EventHandler(this.color1_Click);
+            // 
+            // color2
+            // 
+            this.color2.BackColor = System.Drawing.Color.Green;
+            this.color2.Location = new System.Drawing.Point(1018, 92);
+            this.color2.Name = "color2";
+            this.color2.Size = new System.Drawing.Size(33, 33);
+            this.color2.TabIndex = 3;
+            this.color2.TabStop = false;
+            this.color2.Visible = false;
+            this.color2.Click += new System.EventHandler(this.color2_Click);
+            // 
+            // color3
+            // 
+            this.color3.BackColor = System.Drawing.Color.Blue;
+            this.color3.Location = new System.Drawing.Point(1057, 92);
+            this.color3.Name = "color3";
+            this.color3.Size = new System.Drawing.Size(33, 33);
+            this.color3.TabIndex = 3;
+            this.color3.TabStop = false;
+            this.color3.Visible = false;
+            this.color3.Click += new System.EventHandler(this.color3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1274, 740);
+            this.ClientSize = new System.Drawing.Size(1300, 900);
+            this.Controls.Add(this.color3);
+            this.Controls.Add(this.color2);
+            this.Controls.Add(this.color1);
+            this.Controls.Add(this.comboBoxLine);
             this.Controls.Add(this.buttonHighlight);
             this.Controls.Add(this.buttonFillImage);
             this.Controls.Add(this.buttonLine);
@@ -108,6 +181,9 @@ namespace RasterAlgorithms
             this.Name = "Form1";
             this.Text = "Пэинт (на минималках)";
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.color3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -120,6 +196,12 @@ namespace RasterAlgorithms
         private System.Windows.Forms.Button buttonHighlight;
         private System.Windows.Forms.Button buttonLine;
         private System.Windows.Forms.Button buttonTriangle;
+        private System.Windows.Forms.OpenFileDialog chooseFileDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.ComboBox comboBoxLine;
+        private System.Windows.Forms.PictureBox color1;
+        private System.Windows.Forms.PictureBox color2;
+        private System.Windows.Forms.PictureBox color3;
     }
 }
 
--- RasterAlgorithms/Form1.cs
+++ RasterAlgorithms/Form1.cs
@@ -12,10 +12,98 @@ namespace RasterAlgorithms
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
+        String currentFileName;
+        Color currentColorFill;
+        Color currentColorTr1;
+        Color currentColorTr2;
+        Color currentColorTr3;
+        bool isVu = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void buttonFillColor_Click(object sender, EventArgs e)
+        {
+            changeVisibility(false, false);
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentColorFill = colorDialog.Color;
+            }
+        }
+
+        private void buttonFillImage_Click(object sender, EventArgs e)
+        {
+            changeVisibility(false, false);
+
+            if (chooseFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFileName = chooseFileDialog.FileName;
+            }
+        }
+
+        private void buttonHighlight_Click(object sender, EventArgs e)
+        {
+            changeVisibility(false, false);
+
+            if (chooseFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFileName = chooseFileDialog.FileName;
+            }
+        }
+
+        private void buttonLine_Click(object sender, EventArgs e)
+        {
+            changeVisibility(true, false);
+            comboBoxLine.SelectedIndex = 0;
+        }
+
+        private void buttonTriangle_Click(object sender, EventArgs e)
+        {
+            changeVisibility(false, true);
+            color1.BackColor = currentColorTr1 = Color.Red;
+            color2.BackColor = currentColorTr2 = Color.Green;
+            color3.BackColor = currentColorTr3 = Color.Blue;
+        }
+
+        private void comboBoxLine_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            canvas.BackColor = SystemColors.Control;
+            isVu = comboBoxLine.SelectedIndex == 1;
+        }
+
+        private void changeVisibility(bool combobox, bool colors)
+        {
+            comboBoxLine.Visible = combobox;
+            color1.Visible = colors;
+            color2.Visible = colors;
+            color3.Visible = colors;
+        }
+
+        private void color1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                color1.BackColor = currentColorTr1 = colorDialog.Color;
+            }
+        }
+
+        private void color2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                color2.BackColor = currentColorTr2 = colorDialog.Color;
+            }
+        }
+
+        private void color3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                color3.BackColor = currentColorTr3 = colorDialog.Color;
+            }
+        }
     }
 }
